1783f889f37ba8749aedc328e70223894e064d3c
github username <github email address>
2018-09-05 22:25:39 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,11 +9,13 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    @genres = Genre.all
   end
 
   def create
     @song = Song.new(song_params)
-
+    # @notes = Note.new(note_params)
+    # byebug
     if @song.save
       redirect_to @song
     else
@@ -47,7 +49,12 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_id, :note_contents => [])
+  end
+
+
+  def note_params
+    params.permit(:notes => [])
   end
 end
 
